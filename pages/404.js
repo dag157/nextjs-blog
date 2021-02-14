07654e2ff3b04000a834b52e2e9f316e36cb121e
dag157 <dag157@pitt.edu>
2021-02-13 17:31:26 -0800

    First Website build

---
+++ pages/404.js
@@ -0,0 +1,9 @@
+import Layout from "../components/layout";
+
+export default function Custom404() {
+    return (
+        <Layout>
+            <h2>Sorry the page you're looking for doesn't exist.</h2>
+        </Layout>
+    )
+  }
