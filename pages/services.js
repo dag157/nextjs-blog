65fa1e644ce825cca57822f1c495d5a25106d3e4
dag157 <dag157@pitt.edu>
2021-03-29 00:31:43 -0700

    services page

--- pages/services.js
+++ pages/services.js
@@ -32,17 +32,36 @@ export default function Services({ allPostsData }) {
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h1 className={utilStyles.headingXl}><RedText str={'Services'}></RedText></h1>
         <div className={utilStyles.centerContent}>
-            <Card img_path='/images/sitemap-solid.svg'>
+            <Card 
+                img_path='/images/sitemap-solid.svg'
+                card_title='Mock Interviews'
+                card_txt='The mock interview sessions include behavioral and technical portions. I offer both 30 minute phone screens and 1 hour technical interviews. After the interview, I will follow up with a detailed report within 24 hours with an evaluation, including improved question responses and resources to help you improve your interviewing skills.'
+                card_height={1008}
+                card_width={820}
+            >
             </Card>
         </div>
         <br />
         <div className={utilStyles.centerContent}>
-            <Card img_path='/images/file-solid.svg' reverse>
+            <Card 
+                img_path='/images/file-solid.svg' 
+                card_title='Resume Review'
+                card_txt='The resume service will include a detailed report on your resume, including ways you can improve it and general feedback. I also offer to help you create a new resume if needed.'
+                card_height={1008}
+                card_width={420}
+                reverse
+            >
             </Card>
         </div>
         <br />
         <div className={utilStyles.centerContent}>
-            <Card img_path='/images/chalkboard-teacher-solid.svg'>
+            <Card 
+                img_path='/images/chalkboard-teacher-solid.svg'
+                card_title='Tutoring'
+                card_txt='Interested in learning how to program? I offer one on one live sessions to teach you the basics. Some general topics include web development, object oriented programming and cloud services.'
+                card_height={1008}
+                card_width={420}
+            >
             </Card>
         </div>
         <br />
